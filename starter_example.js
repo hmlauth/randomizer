6793231726ef449ec09293acf40c7d6ff50b3445
Andromeda <hannalauth@gmail.com>
2019-10-08 17:49:14 -0700

    minor bug fix

--- starter_example.js
+++ starter_example.js
@@ -1,7 +1,7 @@
 const {init, createGroups} = require('./index');
 
-const allStudents = init.allStudents('students_roster.txt');
+const allStudents = init.allStudents('students_roster_example.txt');
 createGroups(4, true, allStudents);
 
-const studentsByStrength = init.byStrength('students_strengths.txt');
+const studentsByStrength = init.byStrength('students_strengths_example.txt');
 createGroups(null, true, studentsByStrength);
