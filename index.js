e7107e124e87cad9172974abed35417a721edee9
Andromeda <hannalauth@gmail.com>
2019-10-09 18:44:25 -0700

    removed console logs

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const { getStudents } = require('./init/index');
 const {createGroups} = require('./create/create');
 
-console.log(getStudents);
-console.log(createGroups);
-
 module.exports = { getStudents, createGroups };
 
 
