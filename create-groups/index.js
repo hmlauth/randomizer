72c2d9b584ccbbc6aaaff57e5b3ba198dd57d0a0
Andromeda <hannalauth@gmail.com>
2019-10-13 11:09:39 -0700

    refactored students by strength based on number of standard students per strong student without needing to loop twice

--- create-groups/index.js
+++ create-groups/index.js
@@ -26,21 +26,44 @@ module.exports = {
 
         const strongStudents = studentList["strong"];
         const standardStudents = studentList["standard"];
+        // console.log(strongStudents);
+        // console.log(standardStudents);
+        const groupSize = Math.floor( standardStudents.length / strongStudents.length );
+        let remainder = standardStudents.length % strongStudents.length;
+        console.log(`${groupSize}, ${remainder}`);
 
         if (shouldShuffle) {
             strongStudents.shuffle();
             standardStudents.shuffle();
         };
 
-        const finalGroups = strongStudents.map(student => [student]);
-        
-        let groupNumber = 0;
+
+        let finalGroups = strongStudents.map(student => [student]);
+
         while (standardStudents.length > 0) {
-            let student = standardStudents.shift(); 
-            finalGroups[groupNumber].push(student);
-            groupNumber === finalGroups.length - 1 ? groupNumber = 0 : groupNumber++;
-        }
+            finalGroups = finalGroups.map(group => {
+                let subGroup;
+
+                if (remainder > 0) {
+                    subGroup = standardStudents.splice(0, groupSize + 1);
+                    remainder--;
+                } else {
+                    subGroup = standardStudents.splice(0, groupSize);
+                }
+        
+                return group.concat(subGroup);
+            });
+        };
         
         return finalGroups
     }
 };
+
+
+
+// standardStudents.length !== remainder ? 
+//                 finalGroups[groupNumber].push(standardStudents.splice(-groupSize)[0]) &&
+//                 groupNumber++ 
+//                 :
+//                 groupNumber = 0 &&
+//                 finalGroups[groupNumber].push(standardStudents.splice(-1)[0]);    
